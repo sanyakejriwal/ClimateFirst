6275417326eda75647c34f69613a11bb407f7e8b
Isha Gosalia <ishagosalia1@gmail.com>
2021-06-29 16:32:40 -0700

    finished up basics page, created segues and classes for each view controller for the info pages

---
+++ ClimateFirst/RisingSeaLevelsPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RisingSeaLevelsPageViewController.swift
+//  ClimateFirst
+//
+//  Created by Isha Gosalia on 6/29/21.
+//
+
+import UIKit
+
+class RisingSeaLevelsPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
