bcdb14e500919fbe7d6fd32d7b1a48b1d66e0c72
isabelle <bellemeza2002@gmail.com>
2021-06-30 11:26:54 -0600

    resources

---
+++ ClimateFirst/resources.swift
@@ -0,0 +1,29 @@
+//
+//  resources.swift
+//  ClimateFirst
+//
+//  Created by Nizhone Meza on 6/30/21.
+//
+
+import UIKit
+
+class resources: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
