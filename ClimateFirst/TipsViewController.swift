ec288de110ac1410da599f2409626b52e0f7a941
Sanya <sanya@purpletrail.com>
2021-06-30 20:37:14 -0700

    worked on how to help page

--- ClimateFirst/TipsViewController.swift
+++ ClimateFirst/TipsViewController.swift
@@ -14,8 +14,52 @@ class TipsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    var count = 0
 
+    @IBOutlet weak var tip1: UISwitch!
+    
+    @IBOutlet weak var tip2: UISwitch!
+    
+    @IBOutlet weak var tip3: UISwitch!
+    
+    @IBOutlet weak var tip4: UISwitch!
+    
+    @IBOutlet weak var tip5: UISwitch!
+    
+    @IBOutlet weak var tip6: UISwitch!
+    
+    @IBOutlet weak var tip7: UISwitch!
+    
+    @IBAction func doneButton(_ sender: UIButton) {
+        if tip1.isOn {
+            count += 1
+        }
+        if tip2.isOn {
+            count += 1
+        }
+        if tip3.isOn {
+            count += 1
+        }
+        if tip4.isOn {
+            count += 1
+        }
+        if tip5.isOn {
+            count += 1
+        }
+        if tip6.isOn {
+            count += 1
+        }
+        if tip7.isOn {
+            count += 1
+        }
+        climateScore.text = "Your Climate Score is: \(count) out of 7"
+        count = 0
+    }
+    
+    @IBOutlet weak var climateScore: UILabel!
+    
+    
+    
     /*
      // MARK: - Navigation
 
